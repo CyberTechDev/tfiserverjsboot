ab462b19a292aace8e936f68bf39a00d9241d66a
Travis <tp523130@gmail.com>
2018-06-28 20:03:46 -0700

    Start.js Update

--- server.js
+++ server.js
@@ -1,37 +1,29 @@
 var http = require('http');
-var fs = require("fs");
-
-http.createServer(function(request, response) {
-
-	if(request.url === "/index"){
-		sendFileContent(response, "index.html", "text/html");
-	}
-	else if(request.url === "/"){
-		response.writeHead(200, {'Content-Type': 'text/html'});
-		response.write('<b>Hey there!</b><br /><br />This is the default response. Requested URL is: ' + request.url);
-	}
-	else if(/^\/[a-zA-Z0-9\/]*.js$/.test(request.url.toString())){
-		sendFileContent(response, request.url.toString().substring(1), "text/javascript");
-	}
-	else if(/^\/[a-zA-Z0-9\/]*.css$/.test(request.url.toString())){
-		sendFileContent(response, request.url.toString().substring(1), "text/css");
-	}
-	else{
-		console.log("Requested URL is: " + request.url);
-		response.end();
-	}
-}).listen(8000);
-
-function sendFileContent(response, fileName, contentType){
-	fs.readFile(fileName, function(err, data){
-		if(err){
-			response.writeHead(404);
-			response.write("Not Found!");
-		}
-		else{
-			response.writeHead(200, {'Content-Type': contentType});
-			response.write(data);
-		}
-		response.end();
-	});
+var fs = require('fs');
+function onRequest(request, response) {
+  response.writeHead(200, {'Content-type': 'text/html'});
+  fs.readFile('./index.html', null, function(error, data) {
+    if (error){
+      response.writeHead('404');
+      response.write('File not found!');
+    } else {
+        response.write(data);
+    }
+    response.end();
+  });
 }
+
+function onRequest(request, response) {
+  response.writeHead(200, {'Content-type': 'text/css'});
+  fs.readFile('./main.css', null, function(error, data) {
+    if (error){
+      response.writeHead('404');
+      response.write('File not found!');
+    } else {
+        response.write(data);
+    }
+    response.end();
+  });
+}
+
+http.createServer(onRequest).listen(8000);
